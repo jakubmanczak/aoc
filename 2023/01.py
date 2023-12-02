64c30d9c6e72553fe9ed1cb8cfd5737dd200c9c7
jakubmanczak <jakub@manczak.net>
2023-12-02 20:22:27 +0100

    fix part 2 :)

--- 2023/01.py
+++ 2023/01.py
@@ -21,13 +21,15 @@ allowed = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine
 for i in range(0, len(content)):
     num1, num1index, num2, num2index = -1, -1, -1, -1
     # DIGIT ANALYSIS
+    j2 = 0
     for j in str(content[i]):
         if j.isdigit():
             if num1 == -1:
                 num1 = int(j)
-                num1index = int(content[i].index(j))
+                num1index = int(content[i].index(j, j2))
             num2 = int(j)
-            num2index = int(content[i].index(j))
+            num2index = int(content[i].index(j, j2))
+        j2 += 1
     # WORD ANALYSIS
     for word in allowed:
         for j in range(0, len(content[i])):
@@ -42,7 +44,4 @@ for i in range(0, len(content)):
     if num1 != -1 and num2 != -1:
         concat = f"{str(num1)}{str(num2)}"
         tally += int(concat)
-        # print(f"{concat}")  # , "OK" if concat in ["29", "83", "13", "24", "42", "14", "76"] else "!!")
-    else:
-        print("!!")
 print(f"The tally for alphanumeric digits is {tally}")
